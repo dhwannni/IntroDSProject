9fd3b19fd947b16c9e802cd49234090c50b7642f
jse84 <jse84@scarletmail.rutgers.edu>
2026-04-30 13:38:12 -0400

    util.py
    Added load_ai_comments and load_human_comments

--- util.py
+++ util.py
@@ -1,16 +1,26 @@
+import pandas as pd
 import re
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 
 
 NON_ALNUM_RE = re.compile(r'[^a-z0-9\s]+') # keeps the spaces 
-EMOJI_RE = re.compile(r'[^\w\s,]')
+EMOJI_RE = re.compile(
+    "["
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F680-\U0001F6FF"  # transport & map
+    "\U0001F1E0-\U0001F1FF"  # flags
+    "]+",
+    flags = re.UNICODE
+)
+
 
 
 def preprocess_text(
     text: str,
-    remove_stopwords: bool = True, 
-    remove_emojis: bool = True,
-    remove_punctuation: bool = True
+    remove_stopwords: bool = False, # Ai usually uses more complete sentences while humans speak in fragments, slang
+    remove_emojis: bool =  False,
+    remove_punctuation: bool = False     #Humans sometimes  use "!!!" or "?!?". AI usually sounds more cleaner 
     ) -> list[str]:
   
   
@@ -47,3 +57,29 @@ def preprocess_text(
     
     
     return tokens
+
+###Loading AI comments 
+
+
+def load_ai_comments (file_path):
+    df = pd.read_csv(file_path)
+
+    comments  = df["comment"].astype(str).tolist()
+    labels = ["ai"] * len(comments)
+
+    #processed = [preprocess_text(c) for c in comments]
+
+    return labels, comments 
+
+
+
+def load_human_comments(file_path):
+    df = pd.read_csv(file_path)
+
+    comments = df["CommentText"].astype(str).tolist()
+    labels = ["human"] * len(comments)
+
+    return comments, labels
+
+
+#combining commets from the data frames 
